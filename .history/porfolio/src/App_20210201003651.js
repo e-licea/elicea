c7b3ef58ead13ed4994bfda27cff3b383be40011
root <root@DESKTOP-AC53C8E.localdomain>
2021-02-01 12:08:00 -0500

    ...

--- .history/porfolio/src/App_20210201003651.js
+++ .history/porfolio/src/App_20210201003651.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>Asshole</code> and save to reload.
         </p>
