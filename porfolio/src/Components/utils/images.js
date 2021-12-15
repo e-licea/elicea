748ce351d327b3f17fd8d41f2dcd0898994e1a0d
e-licea <elicea0412@gmail.com>
2021-12-15 22:08:20 +0100

    fixed png images. used import instead of require

--- porfolio/src/Components/utils/images.js
+++ porfolio/src/Components/utils/images.js
@@ -1,17 +1,28 @@
+import hgrMockup from '../../img/slides/hgrMockupFinal.png'
+import rTeamMockup from '../../img/slides/rTeamMockup.png'
+import reactSnkrsMockup from '../../img/slides/reactSnkrs.PNG'
+import articleManagerMockup from '../../img/slides/articleManager.png'
+import desktop from '../../img/theLab/desktop.jpg'
+import previous from '../../img/slides/previous.png'
+import next from '../../img/slides/next.png'
+import copy_dark from '../../img/icons/light/copy_light.png'
+import copy_light from '../../img/icons/dark/copy_dark.png'
+import loading from '../../img/slides/loading.gif'
 
 
 const images = {
 
-    hgrMockup: require('../../img/slides/hgrMockupFinal.png').default,
+    hgrMockup: hgrMockup,
     rTeamMockup: require('../../img/slides/rTeamMockup.png').default,
-    reactSnkrsMockup: require('../../img/slides/reactSnkrs.PNG').default,
-    articleManagerMockup: require('../../img/slides/articleManager.png').default,
-    desktop: require('../../img/theLab/desktop.jpg').default,
-    previous: require('../../img/slides/previous.png').default,
-    next: require('../../img/slides/next.png').default,
-    loading: require('../../img/slides/loading.gif').default,
-    copy_dark: require('../../img/icons/dark/copy_dark.png').default,
-    copy_light: require('../../img/icons/light/copy_light.png').default
+    reactSnkrsMockup: reactSnkrsMockup,
+    rTeamMockup: rTeamMockup,
+    articleManagerMockup: articleManagerMockup,
+    desktop: desktop,
+    previous: previous,
+    next: next,
+    loading: loading,
+    copy_dark: copy_dark,
+    copy_light: copy_light
 
 }
 export default images;
