36c726d1d3373cbbf0df31cdbc07e1e3d39b4fe7
e-licea <elicea0412@gmail.com>
2022-01-11 06:32:54 +0100

    ...

--- porfolio/src/Components/Pages/SlideShow.jsx
+++ porfolio/src/Components/Pages/SlideShow.jsx
@@ -51,9 +51,11 @@ export default function SlideShow(){
 
       <div className ='project-overview' key = {activeProject.id}>
         <h4>{activeProject.project}</h4>
-        <p>
-          {activeProject.desc}
-        </p>
+        {
+          activeProject.desc.map(p=>{
+            return <p>{p}</p>
+          })
+        }
         <h6>Challenges...</h6>
         <ul>
         {activeProject.challenges.map(index=><li>{index}</li>)}
@@ -108,7 +110,13 @@ export const projects = [
     deploy:'https://www.highgroundrestore.com',
     src:'https://github.com/e-licea/company/tree/master, https://github.com/e-licea/redux-teammanager-api',
     imgSrc: images.hgrMockup,
-    desc:'High Ground Restore is a roofing start up that is continuously growing. It aims to build strong branding from scratch that is built upon education and community. It is a front-end React application that utilizes NextJS framework for better SEO analytics. High Ground Restore is not just a company that offers roofing services, but also roofing education through it\'s wide variety of articles that are served through their back-end API that is made with Flask. This application was built with a long-term marketing goal to be able to serve customers to their satisfaction as well as keeping them updated on the topic of roofing.',
+    desc:[
+      'High Ground Restore is a roofing start up that is continuously growing. I work closely with the company CEO to make sure this project is tailored for what High Ground Restore needs. Upon a consultation, I found out that High Ground Restore aims to not only provide services to their community, but educate them as well. This is why I was able to build branding from scratch to really capture their personality and the character they want to portray.',
+      'This application was built with ReactJS, but was recently migrated to NextJS for better SEO optimization. The migration entails shrinking images to their corresponding viewport to fetch images faster, dynamic page titles, meta descriptions for search engines, and much more. The site\'s SEO is constantly being monitored, tested, and changed to keep up with competitors in the Chicagoland area.',
+      'In addition, the application has it\'s own back-end that will service articles to visitors through it\'s own api. The api is a Flask server, which is built as a blog manager. You can read more about how this api works in detail by finding the `Flask Blog Manager Project` in this page.',
+      'Visit this project in it\'s entirety with the deploy link below! '
+    
+    ],
     challenges: ['Brand Creation', 'NextJS Migration', 'Setting up Flask Server'],
     technologies: ['React', 'NextJS', 'Flask', 'Weather API', 'Less CSS', 'Context API'],
     currentlyWorking: ['SEO Implementation','Content Creation', 'Selling Ad Space']
@@ -118,7 +126,9 @@ export const projects = [
     project: 'Sneaker Tool',
     src:'https://github.com/e-licea/react-snkrz',
     imgSrc: images.reactSnkrsMockup,
-    desc:'This app is a sneaker tool that is made for the most passionate sneaker fanatics. It\'s intent is to serve exclusive information that will help sneaker fanatics prepare for their next anticipated shoe release. It serves information such as retail pricing, release dates, colorways, and even a prediction on what sneaker organizations will be releasing which shoe. Even when no sneaker releases are taking place, users are still able to take advantage of the large sneakerpedia that this sneaker tool offers. A database is able to serve insights on almost any sneaker that is searched by the user. The convenience of such a tool can come in handy for people who make hustles from shoe resales or simply just to pull up a sneaker information to show a friend.',
+    desc:[
+      'This app is a sneaker tool that is made for the most passionate sneaker fanatics. It\'s intent is to serve exclusive information that will help sneaker fanatics prepare for their next anticipated shoe release. It serves information such as retail pricing, release dates, colorways, and even a prediction on what sneaker organizations will be releasing which shoe. Even when no sneaker releases are taking place, users are still able to take advantage of the large sneakerpedia that this sneaker tool offers. A database is able to serve insights on almost any sneaker that is searched by the user. The convenience of such a tool can come in handy for people who make hustles from shoe resales or simply just to pull up a sneaker information to show a friend.'
+    ],
     challenges: ['Web Scraping and parsing information to JSON with async'],
     technologies: ['React', 'Cheerio', 'Node', 'Express',  'Less CSS'],
     currentlyWorking: ['Deploying on a server with multiple open ports']
@@ -129,7 +139,7 @@ export const projects = [
     src:'https://github.com/e-licea/react-snkrz',
     deploy: 'https://dreamy-pare-fd4639.netlify.app/',
     imgSrc: images.rTeamMockup,
-    desc:'The idea behind the Social Platform was creating a workspace much like Discord and Slack. In my experience I have used both platforms and became attracted to the way these services flowed. I became inspired to try creating my own workspace platform like these with a slight touch of Facebook. I think the hardest part about this project was building a minimalistic database schema that would make sense for the entire application. Although state management is a bit tedious, Redux was a great help. Currently, this application is being optimized and chat functionality is being constructed but a beta demo can be tested with the deploy link down below. Have at it!',
+    desc:['The idea behind the Social Platform was creating a workspace much like Discord and Slack. In my experience I have used both platforms and became attracted to the way these services flowed. I became inspired to try creating my own workspace platform like these with a slight touch of Facebook. I think the hardest part about this project was building a minimalistic database schema that would make sense for the entire application. Although state management is a bit tedious, Redux was a great help. Currently, this application is being optimized and chat functionality is being constructed but a beta demo can be tested with the deploy link down below. Have at it!'],
     challenges: ['Redux state', 'Authentication with JSON Web Tokens','Creating a reliable emoji api', 'Creating a logical Postgres database schema' ],
     technologies: ['React', 'Redux', 'Node','Yup Form Validator', 'Express', 'Less CSS', 'PostgreSQL'],
     currentlyWorking:['Creating user profile', 'Setting up a status feed page.']
@@ -140,7 +150,7 @@ export const projects = [
     src:'https://github.com/e-licea/highgroundrestoreapi',
     deploy: 'https://highgroundrestoreapi.herokuapp.com/login',
     imgSrc: images.articleManagerMockup,
-    desc:'This Flask application was designed for managing and creating content for a blog website. The blog manager has protected endpoints with Flask sessions that require the proper credentials for write privileges inside the app. Once logged in, a user is able to create drafts, add content such as paragraphs and images, and then publish the article. When the article is published, it is immediately available to be read through the client api endpoints.',
+    desc:['This Flask application was designed for managing and creating content for a blog website. The blog manager has protected endpoints with Flask sessions that require the proper credentials for write privileges inside the app. Once logged in, a user is able to create drafts, add content such as paragraphs and images, and then publish the article. When the article is published, it is immediately available to be read through the client api endpoints.'],
     challenges: ['Authentication with Flask Sessions','Serializing data to JSON', 'Creating a logical Postgres database schema' ],
     technologies: ['Flask', 'Vanilla JS', 'DJango HTML', 'SQL Alchemy',  'Less CSS', 'PostgreSQL'],
     currentlyWorking:['Adding additional edit functions']
