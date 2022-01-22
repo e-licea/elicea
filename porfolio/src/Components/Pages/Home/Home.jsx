838ea962ea7dea9a4bb2fc69d610b4de7902e5c7
e-licea <elicea0412@gmail.com>
2022-01-22 17:26:51 +0100

    ...

--- porfolio/src/Components/Pages/Home/Home.jsx
+++ porfolio/src/Components/Pages/Home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
             <Jumbotron/>
             <Featured/>
             <Skills/>
-            <Intro/>
+            {/* <Intro/> */}
             <Heatmap/>
         </div>
     )
